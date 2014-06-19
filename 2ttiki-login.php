7c4d2fccfbbeaa1b063ca456116d870bd68522ff
shailendraatrey <shailendra.atrey@gmail.com>
2014-06-19 11:33:43 +0530

    deleted

--- 2ttiki-login.php
+++ 2ttiki-login.php
@@ -1,17 +1,20 @@
 <?php
-$client_ID = $_REQUEST['clientid'];
-$user_ID = $_REQUEST['userid'];
+
+//$req_uri = explode("/",$_SERVER['REQUEST_URI']); 
+//$client_code = $req_uri[1];
+$client_code = $_REQUEST['client'];
+$user = $_REQUEST['user'];
+$password = $_REQUEST['pass'];
 $wsdl = "http://a19c32c9f6734e2c910e05c5d64ebed6.cloudapp.net/services/CloudTikiWikiService.svc?wsdl";
-$params = array('userID'=>$user_ID,'ClientID'=>$client_ID);
+$params = array('UserName'=>$user,'Password'=>$password,'ClientCode'=>$client_code,'FailedLoginCount'=>"0");
 $client = new SoapClient($wsdl);
-$json_result = $client->GetLoggedInUserDetails($params);
+$json_result = $client->LogOn($params);
 $json_obj = json_decode($json_result->LogOnResult);
 $is_valid_login = $json_obj->{'IsValidLoggin'}; 
 if($is_valid_login) {
 	$email = $json_obj->{'EmailID'}; 
-     $user = $json_obj->{'UserName'}; 
 	$is_admin = $json_obj->{'IsSuperAdmin'}; 
-	if(isset($_REQUEST['userid']) && $_REQUEST['userid']!="") {
+	if(isset($_REQUEST['user']) && $_REQUEST['user']!="") {
 		$_POST['user'] = $user ;
 		$_POST['pass'] = 'dummy';
 		$_POST['email'] = $email;
